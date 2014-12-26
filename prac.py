678e6e684158b60800542d7f9d78b7946fb5fb97
Mikhail Gordeev <gordmisha@gmail.com>
2014-12-26 12:14:45 +0300

    Function

--- prac.py
+++ prac.py
@@ -167,36 +167,69 @@ for name, val in nodeDB.items():
 
 def dslice(d, l): return dict(filter(lambda item: item[0] in l, d.iteritems()))
 
-freeCells = set([x.name for x in nodeDB.values() if not x.terminal])
-gmax = -1
-modCells = []
-for i in xrange(len(nodeDB)):
-    #~ print max(dslice(g, freeCells).values())
-    val = nodeDB[dslice(g, freeCells).keys()[numpy.argmax(dslice(g, freeCells).values())]]
-    badCells = set()
-    #~ print area(A - {val.name}) in xrange(Amin, Amax + 1)
-    while (area(A - {val.name}) not in xrange(Amin, Amax + 1) or len(freeCells - badCells) == 0):
-        #print val.pos
-        badCells |= {val.name}
-        tmp = freeCells - badCells
-        val = nodeDB[dslice(g, tmp).keys()[numpy.argmax(dslice(g, tmp).values())]]
-    if (len(freeCells - badCells) == 0):
-        break
-    #~ print val.name
-    if (g[val.name] > gmax):
-        gmax = g[val.name]
-    modCells.append(val.name)
-    val.pos ^= 1
-    A ^= {val.name}
-    B ^= {val.name}
-    for name in set([y for x in val.nets for y in x]):
-        g[name] = gain(nodeDB[name])
-    freeCells -= {val.name}
+def FM(nodeDB):
+    A = set()
+    
+    for x in nodeDB.values():
+        if randint(0,1):
+            A.add(x.name)
+    
+    def area(A):
+        s = 0
+        for x in A:
+            s += nodeDB[x].xSize * nodeDB[x].ySize
+        
+        return s
+    
+    vmax = max([x.xSize * x.ySize for x in nodeDB.values()])
+    Amin = area(A) - vmax
+    Amax = Amin + 2 * vmax
+    g = {}
 
-for x in nodeDB.values():
-    if x.name in modCells[gmax + 1:]:
-        x.pos ^= 1
+    def gain(c, A):
+        FS, TE = 0, 0
+        for net in c.nets:
+            netpos = set([nodeDB[x].name in A for x in net])
+            if len(netpos) == 1:
+                if netpos.pop() == c.name in A:
+                    TE += 1
+                else:
+                    FS += 1
+        return FS - TE
 
+    for name, val in nodeDB.items():
+        g[name] = gain(val, A)
+    
+    freeCells = set([x.name for x in nodeDB.values()])
+    #~ V = dslice(nodes, freeCells)
+    gmax = -1
+    modCells = []
+    for i in xrange(len('dwV')):
+        #~ print max(dslice(g, freeCells).values())
+        val = nodeDB[dslice(g, freeCells).keys()[numpy.argmax(dslice(g, freeCells).values())]]
+        badCells = set()
+        #~ print area(A - {val.name}) in xrange(Amin, Amax + 1)
+        while (area(A - {val.name}) not in xrange(Amin, Amax + 1) or len(freeCells - badCells) == 0):
+            #print val.pos
+            badCells |= {val.name}
+            tmp = freeCells - badCells
+            val = nodeDB[dslice(g, tmp).keys()[numpy.argmax(dslice(g, tmp).values())]]
+        if (len(freeCells - badCells) == 0):
+            break
+        #~ print val.name
+        if (g[val.name] > gmax):
+            gmax = g[val.name]
+        modCells.append(val.name)
+        A ^= {val.name}
+        for name in set([y for x in val.nets for y in x]):
+            g[name] = gain(nodeDB[name], A)
+        freeCells -= {val.name}
+    for x in nodeDB.values():
+        if x.name in modCells[gmax + 1:]:
+            A ^= {x.name}
+    return A
+
+print "Size of A partition: {}".format(len(FM(nodeDB)))
 
 print "Time: {}".format(clock() - beginTime)
 
